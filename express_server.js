ce5d5eb27c880814dc5fa2ebd5f59ae7ba0a392b
Linh Hoang <ellehoang@hotmail.com>
2021-11-10 14:49:21 -0500

    set ejs as view engine in express_server.js

--- express_server.js
+++ express_server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set('view engine', 'ejs'); // tells Express app to use EJS as templating engine
+
 const urlDatabase = {
   "b2xVn2": 'http://www.lighthouselabs.ca',
   "9sm5xK": 'http://www.google.com'
 };
 
-app.get('/', (req, res) => { // register handler on root path, '/'
+app.get('/', (req, res) => { // register handler on root path(home page), '/'
   res.send('Hello!');
 });
 app.get('/urls.json', (req, res) => { // add route/endpoint
