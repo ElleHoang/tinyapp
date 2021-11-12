a1e13a809fa4403f4855f93f03111a2fbec6484c
Linh Hoang <ellehoang@hotmail.com>
2021-11-12 15:34:10 -0500

    Add Logout form to partial template, pass username to GET new route in server, and add POST logout route to server

--- express_server.js
+++ express_server.js
@@ -36,7 +36,8 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
@@ -63,6 +64,10 @@ app.post("/urls/:id", (req, res) => {
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie("username", username);
+  res.redirect("/urls");
+});
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 app.listen(PORT, () => {
