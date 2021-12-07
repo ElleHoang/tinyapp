aee80d0882400839135ba107f2cf723dfe1e26bf
Linh Hoang <ellehoang@hotmail.com>
2021-12-06 22:52:36 -0500

    move newUser, generateRandomString, and urlsForUser function to helper.js

--- helpers.js
+++ helpers.js
@@ -7,4 +7,34 @@ const getUserByEmail = (email, userDatabase) => {
   return undefined;
 };
 
-module.exports = { getUserByEmail };
+const newUser = (email, password) => {
+  const hashPassword = bcrypt.hashSync(password, 10);
+  const userStr = generateRandomString();
+  users[userStr] = {
+    id: userStr,
+    email,
+    password: hashPassword
+  };
+  return userStr;
+};
+
+const generateRandomString = () => {
+  let shortURL = '';
+  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < 6; i++) {
+    shortURL += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return shortURL;
+};
+
+const urlsForUser = (id, urlDatabase) => {
+  const userURL = {};
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      userURL[shortURL] = urlDatabase[shortURL].longURL;
+    }
+  }
+  return userURL;
+};
+
+module.exports = { getUserByEmail, newUser, generateRandomString, urlsForUser };
